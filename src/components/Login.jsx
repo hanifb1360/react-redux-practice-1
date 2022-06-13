b35d07347799b373ec4815d71fecf2f54cd4ceb3
hanifb1360 <hanif.bahari@gmail.com>
2022-06-14 00:19:47 +0200

    logout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
 
 function Login() {
+  const dispatch = useDispatch();
   return (
     <div>
-        <button>Login</button>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "", age: 0, email: "" }));
+        }}
+      >
+        Login
+      </button>
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Logout
+      </button>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/features/user.js
+++ src/features/user.js
@@ -1,15 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
+const initialStateValue = { name: "", age: 0, email: "" };
 export const userSlice = createSlice({
-    name: "user",
-    initialState: { value: {name: "", age: 0, email: ""}},
-    reducers: {
-        login: (state, action) => {
-            state.value = action.payload
-        },
-
+  name: "user",
+  initialState: { value: initialStateValue },
+  reducers: {
+    login: (state, action) => {
+      state.value = action.payload;
     },
-});
-export const { login } = userSlice.actions;
-export default userSlice.reducer;
 
+    logout: (state) => {
+      state.value = initialStateValue;
+    },
+  },
+});
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
